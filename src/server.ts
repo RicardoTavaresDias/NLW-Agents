10a0622205be3d267a2aab05874c7cc48288ff92
RicardoTavaresDias <ricardo.ti.sistemas@gmail.com>
2025-07-10 15:58:25 -0300

    version: 1.1.0 - project completed

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import { fastify } from "fastify"
-import { sql } from './db/connection.ts'
+import { fastifyMultipart } from '@fastify/multipart'
 
 import {
   serializerCompiler,
@@ -12,12 +12,15 @@ import { getRoomsRoute } from "./http/routes/get-rooms.ts"
 import { createRoomRoute } from "./http/routes/create-room.ts"
 import { getRoomQuestions } from "./http/routes/get-room-questions.ts"
 import { createQuestionRoute } from "./http/routes/create-question.ts"
+import { uploadAudioRoute } from "./http/routes/upload-audio.ts"
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.register(fastifyCors, {
   origin: "http://localhost:5173",
 })
+
+app.register(fastifyMultipart)
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
@@ -30,6 +33,7 @@ app.register(getRoomsRoute)
 app.register(createRoomRoute)
 app.register(getRoomQuestions)
 app.register(createQuestionRoute)
+app.register(uploadAudioRoute)
 
 app.listen({ port: env.POST }).then(() => {
   console.log("HTTP server running!")
